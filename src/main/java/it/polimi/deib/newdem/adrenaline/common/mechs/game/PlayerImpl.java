668f2b5eb3f8ba2eb2f5af5cde2e1f84b00e0877
Antonio Lopardo <antonio.lopardo@mail.polimi.it>
2019-03-30 19:18:47 +0100

    add PlayerImpl

---
+++ src/main/java/it/polimi/deib/newdem/adrenaline/common/mechs/game/PlayerImpl.java
@@ -0,0 +1,105 @@
+package it.polimi.deib.newdem.adrenaline.common.mechs.game;
+
+import it.polimi.deib.newdem.adrenaline.common.mechs.actions.ActionFactory;
+import it.polimi.deib.newdem.adrenaline.common.mechs.items.PowerUpSet;
+import it.polimi.deib.newdem.adrenaline.common.mechs.items.WeaponSet;
+
+import java.util.List;
+
+public class PlayerImpl implements Player {
+    @Override
+    public Game getGame() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public PlayerInventory getInventory() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public PlayerColor getColor() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ActionBoard getActionBoard() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public DamageBoard getDamageBoard() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public List<ActionFactory> getMoves() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public int getDeaths() {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public int getTotalDamage() {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public Player getDamager(int cell) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public int getDamageFromPlayer(Player player) {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public int getMarksFromPlayer(Player player) {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public WeaponSet getReadyWeapons() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public WeaponSet getUnloadedWeapons() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public PowerUpSet getPowerUp() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public boolean isDead() {
+        //TODO
+        return false;
+    }
+}
