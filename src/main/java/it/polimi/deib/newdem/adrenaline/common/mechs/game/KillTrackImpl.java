eb5705521fd109ffb33535a0bb683d422b7c4430
Antonio Lopardo <antonio.lopardo@mail.polimi.it>
2019-04-02 17:20:22 +0200

    add KillTrackImpl in game

---
+++ src/main/java/it/polimi/deib/newdem/adrenaline/common/mechs/game/KillTrackImpl.java
@@ -0,0 +1,25 @@
+package it.polimi.deib.newdem.adrenaline.common.mechs.game;
+
+import java.util.List;
+
+public class KillTrackImpl implements KillTrack {
+
+    private List<Player> killers;
+    private int trackLength;
+
+    public KillTrackImpl(int trackLength){
+        //TODO
+    }
+
+    @Override
+    public Player getKiller(int killIndex) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public int getTrackLength() {
+        //TODO
+        return 0;
+    }
+}
