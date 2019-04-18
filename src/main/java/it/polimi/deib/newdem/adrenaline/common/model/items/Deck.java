217faa334dbba7fecc36b66bbb00d55053d75bce
Riccardo Mencucci <riccardo.mencucci@mail.polimi.it>
2019-04-18 23:20:17 +0200

    Add tests and exception for common.items.Deck

--- src/main/java/it/polimi/deib/newdem/adrenaline/common/model/items/Deck.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/common/model/items/Deck.java
@@ -4,32 +4,54 @@ import java.util.List;
 
 public class Deck<T> {
 
-    // subject to change or restructuring
-    private List<T> cards;
+    private List<T> drawableCards;
+    private List<T> discardedCards;
+    private List<T> activeCards;
 
-    /** Creates a new {@code Deck} made up of the elements of the given list
+    /** Creates a new {@code Deck} made up of the elements of the given list.
      *
-     * @param list the elements to build a deck from
+     * @throws IllegalArgumentException {@code list} is null or empty
+     * @param list the elements to build a {@code Deck} from. This may have repetitions.
+     *             Not null, not empty.
      */
     public Deck(List<T> list) {
-        this.cards = list;
+        this.drawableCards = list;
         // TODO implement
     }
 
-    public T extract(){
+    /** Draws a card from this deck.
+     *
+     * If no card is drawable, the discard pile is shuffled into the deck,
+     * then a card is drawn.
+     *
+     * @return the drawn card
+     */
+    public T draw() throws NoDrawableCardException {
         // TODO implement
         return null;
     }
 
-    public void discard(T card){
+    /** Puts the given {@code card} into this deck's discard pile.
+     *
+     * @throws IllegalArgumentException the given {@code card} does not belong to this deck.
+     * @param card the card to discard. Not null, must belong to this deck.
+     */
+    public void discard(T card) {
         // TODO implement
     }
 
+    /** Checks for the presence of drawable cards.
+     *
+     * @return whether or not the deck is empty
+     */
     public boolean isEmpty() {
         // TODO implement
         return true;
     }
 
+    /** Shuffles this deck's discard pile together with the main deck.
+     *
+     */
     public void reshuffle() {
         // TODO implement
     }
---
+++ src/main/java/it/polimi/deib/newdem/adrenaline/common/model/items/NoDrawableCardException.java
@@ -0,0 +1,12 @@
+package it.polimi.deib.newdem.adrenaline.common.model.items;
+
+public class NoDrawableCardException extends Exception {
+
+    public NoDrawableCardException() {
+        super();
+    }
+
+    public NoDrawableCardException(String message) {
+        super(message);
+    }
+}
