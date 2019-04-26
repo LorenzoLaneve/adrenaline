ef005112efe77e26834c486cfe43a342e2cc468d
Antonio Lopardo <antonio.lopardo@mail.polimi.it>
2019-04-26 22:42:46 +0200

    add tested implementation of common.model.map MapBuilder

---
+++ src/test/java/it/polimi/deib/newdem/adrenaline/common/model/map/TestMapBuilder.java
@@ -0,0 +1,29 @@
+package it.polimi.deib.newdem.adrenaline.common.model.map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestMapBuilder {
+
+    @Test
+    public void testConstructor(){
+
+    }
+
+    @Test
+    public void testBuildMatrixMap() {
+    }
+
+    @Test
+    public void testExtractRoomList() {
+    }
+
+    @Test
+    public void testExtractSpawnPointList() {
+    }
+
+    @Test
+    public void testBindTiles() {
+    }
+}
