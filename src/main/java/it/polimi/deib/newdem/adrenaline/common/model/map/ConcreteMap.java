4090968fbf9f3b0b96f4a91c982ae0100baf03b4
Antonio Lopardo <antonio.lopardo@mail.polimi.it>
2019-04-26 22:44:19 +0200

    updated the implementation of common.model.map.ConcreteMap to make use of MapBuilder.

--- src/main/java/it/polimi/deib/newdem/adrenaline/common/model/map/ConcreteMap.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/common/model/map/ConcreteMap.java
@@ -4,49 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConcreteMap implements Map {
-    // TODO figure out data structure
 
-    private ArrayList<ConcreteRoom> rooms;
-    List<RoomTileSeeds> roomTileSeedsList;
+    private List<Room> rooms;
+    private Tile[][] matrixMap;
 
-    public ConcreteMap(List<RoomTileSeeds> roomTileSeedsList) {
-        this.rooms = new ArrayList<>();
-        this.roomTileSeedsList = roomTileSeedsList;
-
+    /**Creates a new {@code ConcreteMap} using the matrixMap and list of rooms provided by the MapBuilder.
+     *
+     * @param matrixMap array of arrays of tiles.
+     * @param rooms list of rooms.
+     */
+    public ConcreteMap(Tile[][] matrixMap, List<Room> rooms) {
+        this.matrixMap = matrixMap;
+        this.rooms = rooms;
     }
 
+    /**Returns the rooms in the map.
+     *
+     * @return the list of rooms belonging to the map.
+     */
     @Override
     public List<Room> getRooms() {
 
         return new ArrayList<>(rooms);
     }
 
+    /**Returns the tile in tilePosition in Map.
+     *
+     * @param tilePosition the position of the tile to return.
+     * @return the tile in tilePosition in Map.
+     */
     @Override
     public Tile getTile(TilePosition tilePosition) {
-        boolean found = false;
         Tile foundTile = null;
 
-        for (ConcreteRoom room : rooms){
-            if(room.getRoomTileSeeds().containsTilePosition(tilePosition)){
-                for(Tile tile : room.getTiles()){
-                    if(tile.getPosition().getY() == tilePosition.getY() &&
-                        tile.getPosition().getX() == tilePosition.getY()){
-                        foundTile = tile;
-                        found = true;
-                    }
-                }
-            }
-        }
+        int x = tilePosition.getX();
+        int y = tilePosition.getY();
+
+        foundTile = matrixMap[x][y];
 
         return foundTile;
     }
 
+    /**Used to set the map of the rooms in rooms since they are created
+     * before the Map to which they belong is initialised. It should always be called right after the constructor.
+     *
+     */
     @Override
-    public void generateRooms() {
-       for(RoomTileSeeds roomTileSeeds : roomTileSeedsList){
-           ConcreteRoom newRoom = new ConcreteRoom(this,roomTileSeeds);
-           newRoom.generateTiles();
-           rooms.add(newRoom);
+    public void bindRooms() {
+        for(Room room : rooms){
+            room.setMap(this);
         }
     }
 }
