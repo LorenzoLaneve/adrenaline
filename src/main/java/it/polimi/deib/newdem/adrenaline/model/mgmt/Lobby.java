16cfefb149390bf6a00dd3b8c0199918a9e60337
Lorenzo Laneve <lore97drk@icloud.com>
2019-05-07 16:50:35 +0200

    implement Lobby and LobbyImpl and add docs accordingly

--- src/main/java/it/polimi/deib/newdem/adrenaline/model/mgmt/Lobby.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/model/mgmt/Lobby.java
@@ -4,9 +4,30 @@ import java.util.List;
 
 public interface Lobby {
 
+    /**
+     * Adds the given user to the lobby.
+     */
     void addUser(User user);
 
+    /**
+     * Removes the given user from the lobby.
+     */
     void removeUser(User user);
 
+    /**
+     * Returns a list of the users that are in the lobby at the moment of the call.
+     * Please note that no assumption will be made on the order.
+     */
     List<User> getUsers();
+
+    /**
+     * Returns the minimum number of players required by the lobby to start the game.
+     */
+    int getMinPlayers();
+
+    /**
+     * Returns the number of players that will cause the lobby to immediately start the game.
+     */
+    int getMaxPlayers();
+
 }
--- src/main/java/it/polimi/deib/newdem/adrenaline/model/mgmt/LobbyImpl.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/model/mgmt/LobbyImpl.java
@@ -1,8 +1,9 @@
 package it.polimi.deib.newdem.adrenaline.model.mgmt;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class LobbyImpl {
+public class LobbyImpl implements Lobby {
 
     private int minPlayers;
 
@@ -10,7 +11,66 @@ public class LobbyImpl {
 
     private List<User> users;
 
+    private LobbyState lobbyState;
+
+    private GameManagerFactory gameManagerFactory;
+
+    /**
+     * Creates a new and empty lobby.
+     * @param minPlayers The minimum players required to start.
+     * @param maxPlayers The number of players that will cause the lobby to immediately start the game.
+     * @param gmf A GameManagerFactory object that will create the manager for the game hosted by the lobby.
+     */
     public LobbyImpl(int minPlayers, int maxPlayers, GameManagerFactory gmf){
-        //TODO
+        this.minPlayers = minPlayers;
+        this.maxPlayers = maxPlayers;
+
+        this.users = new ArrayList<>();
+
+        this.gameManagerFactory = gmf;
+
+        this.switchState(new ReadyLobbyState());
     }
+
+    private void switchState(LobbyState newState) {
+        if (newState != this.lobbyState) {
+            if (lobbyState != null) {
+                lobbyState.lobbyDidExitState(this);
+            }
+
+            newState.lobbyWillEnterState(this);
+        }
+
+        this.lobbyState = newState;
+    }
+
+    @Override
+    public void addUser(User user) {
+        this.users.add(user);
+
+        this.switchState(lobbyState.userDidEnterLobby(user, this));
+    }
+
+    @Override
+    public void removeUser(User user) {
+        this.users.remove(user);
+
+        this.switchState(lobbyState.userDidExitLobby(user, this));
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return new ArrayList<>(users);
+    }
+
+    @Override
+    public int getMinPlayers() {
+        return minPlayers;
+    }
+
+    @Override
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
 }
