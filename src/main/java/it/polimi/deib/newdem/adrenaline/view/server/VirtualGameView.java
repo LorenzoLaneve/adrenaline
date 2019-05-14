52cec02dd81a00059015cbdd5ee9d40dd6d83605
Lorenzo Laneve <lore97drk@icloud.com>
2019-05-14 14:41:54 +0200

    Add getUserForColor() to GameView

--- src/main/java/it/polimi/deib/newdem/adrenaline/view/server/VirtualGameView.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/view/server/VirtualGameView.java
@@ -2,12 +2,19 @@ package it.polimi.deib.newdem.adrenaline.view.server;
 
 import it.polimi.deib.newdem.adrenaline.model.game.Game;
 import it.polimi.deib.newdem.adrenaline.model.game.GameListener;
+import it.polimi.deib.newdem.adrenaline.model.game.PlayerColor;
+import it.polimi.deib.newdem.adrenaline.model.mgmt.User;
 import it.polimi.deib.newdem.adrenaline.view.GameView;
 import it.polimi.deib.newdem.adrenaline.view.MapView;
 import it.polimi.deib.newdem.adrenaline.view.PlayerView;
 
 public class VirtualGameView implements GameView, GameListener {
 
+
+    @Override
+    public User getUserForColor(PlayerColor color) {
+        return null;
+    }
 
     @Override
     public void addPlayerView(PlayerView pv) {
@@ -28,4 +35,5 @@ public class VirtualGameView implements GameView, GameListener {
     public void gameWillEnd(Game game) {
         // TODO
     }
+
 }
