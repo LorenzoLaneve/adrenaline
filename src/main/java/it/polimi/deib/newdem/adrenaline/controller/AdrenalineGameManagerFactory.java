91f56b4c33363d113e4c3687eed54d419c908ce0
Lorenzo Laneve <lore97drk@icloud.com>
2019-05-14 14:46:04 +0200

    Move GameManagerFactory to controller package and create AdrenalineGameManagerFactory

---
+++ src/main/java/it/polimi/deib/newdem/adrenaline/controller/AdrenalineGameManagerFactory.java
@@ -0,0 +1,11 @@
+package it.polimi.deib.newdem.adrenaline.controller;
+
+public class AdrenalineGameManagerFactory implements GameManagerFactory {
+
+    @Override
+    public GameManager makeGameManager() {
+        // TODO
+        return null;
+    }
+
+}
--- src/main/java/it/polimi/deib/newdem/adrenaline/model/mgmt/GameManagerFactory.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.deib.newdem.adrenaline.model.mgmt;
-
-import it.polimi.deib.newdem.adrenaline.controller.GameManager;
-
-public interface GameManagerFactory {
-
-    GameManager makeGameManager();
-
-}
