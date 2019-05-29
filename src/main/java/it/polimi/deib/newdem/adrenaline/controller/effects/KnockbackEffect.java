11f5c4354550157c5c4f5338e61ad7385fd1829a
Antonio Lopardo <antonio.lopardo@mail.polimi.it>
2019-05-29 14:17:03 +0200

    update KnockbackEffect

--- src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/KnockbackEffect.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/KnockbackEffect.java
@@ -1,11 +1,13 @@
 package it.polimi.deib.newdem.adrenaline.controller.effects;
 
+import it.polimi.deib.newdem.adrenaline.controller.effects.selection.TileSelectorFactory;
+
 public class KnockbackEffect extends ConcreteEffect {
 
     private int minDistance;
     private int maxDistance;
 
-    public KnockbackEffect(int id, int minDist, int maxDist) {
+    public KnockbackEffect(int id, MetaPlayer target, int knockBackDist, TileSelectorFactory selectorFactory) {
         super(id);
         //TODO
     }
