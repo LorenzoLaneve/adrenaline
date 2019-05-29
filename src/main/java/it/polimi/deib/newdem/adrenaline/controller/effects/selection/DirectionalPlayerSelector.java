59f8a73d759da11475d9d11234bdf997866c1d17
Antonio Lopardo <antonio.lopardo@mail.polimi.it>
2019-05-29 15:22:41 +0200

    implement isSelectable in DirectionalPlayerSelector

--- src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/selection/DirectionalPlayerSelector.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/selection/DirectionalPlayerSelector.java
@@ -3,20 +3,33 @@ package it.polimi.deib.newdem.adrenaline.controller.effects.selection;
 import it.polimi.deib.newdem.adrenaline.model.game.player.Player;
 import it.polimi.deib.newdem.adrenaline.model.map.Direction;
 import it.polimi.deib.newdem.adrenaline.model.map.Map;
+import it.polimi.deib.newdem.adrenaline.model.map.Tile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DirectionalPlayerSelector implements PlayerSelector {
 
     private Player sourcePlayer;
     private Direction direction;
+    private boolean ignoreWalls;
 
-    public DirectionalPlayerSelector(Player sourcePlayer, Direction direction) {
+    public DirectionalPlayerSelector(Player sourcePlayer, Direction direction, boolean ignoreWalls) {
         this.sourcePlayer = sourcePlayer;
         this.direction = direction;
+        this.ignoreWalls = ignoreWalls;
     }
 
     @Override
     public boolean isSelectable(Map map, Player player) {
-        // TODO
-        return false;
+
+        List<Tile> tilesSelectablePlayers = sourcePlayer.getTile().getTiles(direction,ignoreWalls);
+
+        List<Player> selectablePlayers = new ArrayList<>();
+
+        for(Tile tile:tilesSelectablePlayers){
+            selectablePlayers.addAll(tile.getPlayers());
+        }
+        return selectablePlayers.contains(player);
     }
 }
