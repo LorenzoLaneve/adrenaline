2efdc9c70d9c0460497479e09a32cffa30354873
Antonio Lopardo <antonio.lopardo@mail.polimi.it>
2019-05-31 17:11:51 +0200

    add NegatedPlayerSelectorFactory and implement DirectionalTileSelector and its Factory.

--- src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/selection/DirectionalTileSelectorFactory.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/selection/DirectionalTileSelectorFactory.java
@@ -1,21 +1,24 @@
 package it.polimi.deib.newdem.adrenaline.controller.effects.selection;
 
 import it.polimi.deib.newdem.adrenaline.controller.effects.EffectVisitor;
+import it.polimi.deib.newdem.adrenaline.model.map.Tile;
 
 public class DirectionalTileSelectorFactory implements TileSelectorFactory {
 
     private int minDistance;
     private int maxDistance;
     private boolean ignoreWalls;
+    private Tile sourceTile;
 
-    public DirectionalTileSelectorFactory(int minDist, int maxDist, boolean ignoreWalls){
+    public DirectionalTileSelectorFactory(Tile sourceTile,int minDist, int maxDist, boolean ignoreWalls){
         this.minDistance = minDist;
         this.maxDistance = maxDist;
         this.ignoreWalls = ignoreWalls;
+        this.sourceTile = sourceTile;
     }
 
     @Override
     public TileSelector makeSelector(EffectVisitor visitor) {
-        return new DirectionalTileSelector(minDistance, maxDistance, ignoreWalls);
+        return new DirectionalTileSelector(sourceTile,minDistance, maxDistance, ignoreWalls);
     }
 }
