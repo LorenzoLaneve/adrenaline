681675927cfb050e99278044eb12421ae6f45c91
Lorenzo Laneve <lore97drk@icloud.com>
2019-06-04 01:22:59 +0200

    Add constructor overload to DirectionalPlayerSelector that takes a particular Direction.

--- src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/selection/DirectionalPlayerSelector.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/selection/DirectionalPlayerSelector.java
@@ -4,6 +4,7 @@ import it.polimi.deib.newdem.adrenaline.model.game.player.Player;
 import it.polimi.deib.newdem.adrenaline.model.map.Direction;
 import it.polimi.deib.newdem.adrenaline.model.map.Map;
 import it.polimi.deib.newdem.adrenaline.model.map.Tile;
+import it.polimi.deib.newdem.adrenaline.view.inet.events.PlayerReconnectEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,21 +12,33 @@ import java.util.List;
 public class DirectionalPlayerSelector implements PlayerSelector {
 
     private Player sourcePlayer;
+    private Direction direction;
     private boolean ignoreWalls;
 
     public DirectionalPlayerSelector(Player sourcePlayer, boolean ignoreWalls) {
+        this(sourcePlayer, null, ignoreWalls);
+    }
+
+    public DirectionalPlayerSelector(Player sourcePlayer, Direction direction, boolean ignoreWalls) {
         this.sourcePlayer = sourcePlayer;
+        this.direction = direction;
         this.ignoreWalls = ignoreWalls;
     }
 
     @Override
     public boolean isSelectable(Map map, Player player) {
 
+        List<Tile> tilesSelectablePlayers = new ArrayList<>();
 
-        List<Tile> tilesSelectablePlayers = sourcePlayer.getTile().getTiles(Direction.WEST,ignoreWalls);
-        tilesSelectablePlayers.addAll(sourcePlayer.getTile().getTiles(Direction.EAST,ignoreWalls));
-        tilesSelectablePlayers.addAll(sourcePlayer.getTile().getTiles(Direction.NORTH,ignoreWalls));
-        tilesSelectablePlayers.addAll(sourcePlayer.getTile().getTiles(Direction.SOUTH,ignoreWalls));
+        if (direction != null) {
+            tilesSelectablePlayers.addAll(sourcePlayer.getTile().getTiles(direction,ignoreWalls));
+        } else {
+            tilesSelectablePlayers.addAll(sourcePlayer.getTile().getTiles(Direction.NORTH,ignoreWalls));
+            tilesSelectablePlayers.addAll(sourcePlayer.getTile().getTiles(Direction.EAST,ignoreWalls));
+            tilesSelectablePlayers.addAll(sourcePlayer.getTile().getTiles(Direction.SOUTH,ignoreWalls));
+            tilesSelectablePlayers.addAll(sourcePlayer.getTile().getTiles(Direction.WEST,ignoreWalls));
+        }
+
 
         List<Player> selectablePlayers = new ArrayList<>();
 
