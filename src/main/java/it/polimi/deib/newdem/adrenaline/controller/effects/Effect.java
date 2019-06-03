05815ac4a438197987221b4c0a1dd43871184e0e
Lorenzo Laneve <lore97drk@icloud.com>
2019-06-04 01:22:05 +0200

    Remove old effect classes and refactor Effect interface

--- src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/Effect.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/Effect.java
@@ -2,15 +2,6 @@ package it.polimi.deib.newdem.adrenaline.controller.effects;
 
 public interface Effect {
 
-    int DYNAMIC = -1;
-
-    int NO_ID = -2;
-
-
-    int getEffectId();
-
-    PaymentInvoice getPrice();
-
     void apply(EffectVisitor visitor) throws UndoException;
 
 }
