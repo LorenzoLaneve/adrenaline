dedab29d70d979f81937ea434cfa3f19abd69a74
Lorenzo Laneve <lore97drk@icloud.com>
2019-06-26 13:16:42 +0200

    Update GameData interface and implementation

--- src/main/java/it/polimi/deib/newdem/adrenaline/model/game/GameData.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/model/game/GameData.java
@@ -1,10 +1,10 @@
 package it.polimi.deib.newdem.adrenaline.model.game;
 
 import it.polimi.deib.newdem.adrenaline.model.game.player.PlayerColor;
-import it.polimi.deib.newdem.adrenaline.model.map.TilePosition;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 public class GameData implements Serializable {
 
@@ -15,14 +15,16 @@ public class GameData implements Serializable {
         POWER_UP
     }
 
-    private class UserColorPair implements Serializable {
+    public class UserColorPair implements Serializable {
 
         private String username;
         private PlayerColor color;
+        private boolean isOnline;
 
-        UserColorPair(String username, PlayerColor color) {
+        UserColorPair(String username, PlayerColor color, boolean isOnline) {
             this.username = username;
             this.color = color;
+            this.isOnline = isOnline;
         }
 
         public String getUsername() {
@@ -33,101 +35,26 @@ public class GameData implements Serializable {
             return color;
         }
 
-    }
-
-    private class TileDropPair implements Serializable {
-
-        private TilePosition tile;
-        private DropType drop1;
-        private DropType drop2;
-        private DropType drop3;
-
-        TileDropPair(TilePosition tile, DropType drop1, DropType drop2, DropType drop3) {
-            this.tile = tile;
-            this.drop1 = drop1;
-            this.drop2 = drop2;
-            this.drop3 = drop3;
-        }
-
-        public TilePosition getTile() {
-            return tile;
-        }
-
-        public DropType getDrop1() {
-            return drop1;
-        }
-
-        public DropType getDrop2() {
-            return drop2;
-        }
-
-        public DropType getDrop3() {
-            return drop3;
-        }
-
-    }
-
-    private class PlayerTilePair implements Serializable {
-
-        private TilePosition tile;
-        private PlayerColor player;
-
-        PlayerTilePair(TilePosition tile, PlayerColor player) {
-            this.tile = tile;
-            this.player = player;
-        }
-
-        public TilePosition getTile() {
-            return tile;
-        }
-
-        public PlayerColor getPlayer() {
-            return player;
-        }
+        public boolean isOnline() { return isOnline; }
 
     }
 
     private ArrayList<UserColorPair> users;
 
-    private ArrayList<TileDropPair> drops;
-
-    private ArrayList<PlayerTilePair> playerLocations;
-
-    private PlayerColor nextTurnPlayer;
-
-    private String mapID;
-
-
     GameData() {
         this.users = new ArrayList<>();
-        this.drops = new ArrayList<>();
-        this.playerLocations = new ArrayList<>();
-
-        this.mapID = null;
-
-        this.nextTurnPlayer = null;
     }
 
-    public void addPlayer(String name, PlayerColor color) {
-        this.users.add(new UserColorPair(name, color));
-    }
-
-    public void setMapID(String mapID) {
-        this.mapID = mapID;
-    }
-
-    public void addDrop(TilePosition tile, DropType drop1, DropType drop2, DropType drop3) {
-        this.drops.add(new TileDropPair(tile, drop1, drop2, drop3));
-    }
-
-    public void setPlayerLocation(PlayerColor player, TilePosition tile) {
-        this.playerLocations.add(new PlayerTilePair(tile, player));
-    }
-
-    public void setNextTurnPlayer(PlayerColor player) {
-        this.nextTurnPlayer = player;
+    void addPlayer(String name, PlayerColor color, boolean isOnline) {
+        this.users.add(new UserColorPair(name, color, isOnline));
     }
 
 
+    /**
+     * Returns a list of players, in the order they should play and starting with the first player.
+     */
+    public List<UserColorPair> getPlayers() {
+        return new ArrayList<>(users);
+    }
 
 }
