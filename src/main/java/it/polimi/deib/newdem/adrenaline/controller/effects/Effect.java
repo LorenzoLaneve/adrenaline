6a939a4201e0863ec134e897e2f5289255238ff8
Lorenzo Laneve <lore97drk@icloud.com>
2019-06-28 13:31:24 +0200

    Add JavaDoc for controller/effects/ package

--- src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/Effect.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/Effect.java
@@ -2,9 +2,20 @@ package it.polimi.deib.newdem.adrenaline.controller.effects;
 
 import it.polimi.deib.newdem.adrenaline.model.game.player.Player;
 
+/**
+ * A simple function that represents the behaviour of a card.
+ * Thanks to reflection, implementations of this interface can be seen as importable assets.
+ * @see EffectManager for possible features that can be used while creating a card effect.
+ */
 @FunctionalInterface
 public interface Effect {
 
+    /**
+     * @param manager The effect manager that provides the context for this effect.
+     * @param actor The player associated to the user that executes this effect.
+     * @throws UndoException if the user requested to undo the last action.
+     * @implNote Once an action (and so the called effect) has completely finished, it is not possible to undo it,
+     */
     void apply(EffectManager manager, Player actor) throws UndoException;
 
 }
--- src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/MetaPlayer.java
+++ src/main/java/it/polimi/deib/newdem/adrenaline/controller/effects/MetaPlayer.java
@@ -1,5 +1,8 @@
 package it.polimi.deib.newdem.adrenaline.controller.effects;
 
+/**
+ * Possible values representing the player colors drawn in cards.
+ */
 public enum MetaPlayer {
     ATTACKER,
     RED,
